de7835755fe4d4f7f827683a8c7d51b463b90a9f
Dishal Kanishka <87950341+Dotax14@users.noreply.github.com>
2023-03-17 01:27:14 +0530

    modified:   Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -4,7 +4,22 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+  int subject_1;
+  int subject_2;
+  float avg=0;
+
+  printf("Enter Marks of Subject -01 :");
+  scanf("%d",&subject_1);
+
+  printf("Enter Marks of Subject -02:");
+  scanf("%d",&subject_2);
+
+  avg=(subject_1+subject_2)/2.0;
+
+  printf("Average of the two marks %.2f",avg);
+
   
   return 0;
 }
